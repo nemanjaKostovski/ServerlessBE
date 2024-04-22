2806c3c05b65757afe4509519fbe5170e68822bd
Nemanja <nemanjakostovski@gmail.com>
2024-04-22 18:08:32 +0200

    added cors *

--- productService/getProductsById.mjs
+++ productService/getProductsById.mjs
@@ -7,12 +7,18 @@ export const getProductsById = async (event) => {
   if (!product) {
     return {
       statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
       body: JSON.stringify({ message: 'Product not found' }),
     };
   }
 
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     body: JSON.stringify(product),
   };
 };
--- productService/getProductsList.mjs
+++ productService/getProductsList.mjs
@@ -3,6 +3,9 @@ import { products } from './products.mjs';
 export const getProductsList = async () => {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     body: JSON.stringify(products),
   };
 };
