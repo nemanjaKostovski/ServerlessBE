a86899ebbf7c9b8f8309410fe18cfb84bdbcaf3c
Nemanja <nemanjakostovski@gmail.com>
2024-05-18 10:59:15 +0200

    task-6

--- importService/importFileParser.mjs
+++ importService/importFileParser.mjs
@@ -4,9 +4,23 @@ import {
   DeleteObjectCommand,
   CopyObjectCommand,
 } from '@aws-sdk/client-s3';
+import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
 import csvParser from 'csv-parser';
 
 const s3Client = new S3Client();
+const sqsClient = new SQSClient();
+
+const sendMessagePromises = [];
+
+const sendMessage = async (queueUrl, messageBody) => {
+  console.log('Sending message to SQS inside');
+  const command = new SendMessageCommand({
+    QueueUrl: queueUrl,
+    MessageBody: messageBody,
+  });
+  console.log('command', command);
+  await sqsClient.send(command);
+};
 
 export async function importFileParser(event, context) {
   const recordPromises = event.Records.map(async (record) => {
@@ -19,23 +33,38 @@ export async function importFileParser(event, context) {
       const { Body } = await s3Client.send(
         new GetObjectCommand(getObjectParams)
       );
+      const queueUrl = process.env.SQS_QUEUE_URL;
 
-      Body.pipe(csvParser())
-        .on('data', (data) => {
-          console.log('Parsed record:', data);
-        })
-        .on('end', async () => {
-          try {
+      return new Promise((resolve, reject) => {
+        Body.pipe(csvParser())
+          .on('data', async (data) => {
+            console.log('Parsed record:', data);
+            try {
+              const sendMessagePromise = sendMessage(
+                queueUrl,
+                JSON.stringify(data)
+              );
+              sendMessagePromises.push(sendMessagePromise);
+            } catch (error) {
+              console.log('Error sending message to SQS:', error);
+              // throw error;
+              reject(error);
+            }
+          })
+          .on('end', async () => {
+            console.log('Sending message to SQS');
+            await Promise.all(sendMessagePromises);
+            console.log('Message sent to SQS');
+            console.log('Copying and deleting file');
             await copyAndDelete(record);
             console.log('CSV file successfully processed');
-          } catch (error) {
-            console.error('Error copying and deleting file:', error);
-            throw error;
-          }
-        })
-        .on('error', (error) => {
-          console.error('Error processing CSV file', error);
-        });
+            resolve();
+          })
+          .on('error', (error) => {
+            console.error('Error processing CSV file', error);
+            reject(error);
+          });
+      });
     } catch (error) {
       console.error('Error getting object from S3:', error);
       throw error;
